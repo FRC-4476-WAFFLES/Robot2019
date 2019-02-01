3b3c2aa8ea3b35fbf1f666677fd2d79ac60fa144
Duncan S <dmiester1965@gmail.com>
2019-02-01 17:11:37 -0500

    Add Elevator Commands

---
+++ 2019RobotTest/src/main/cpp/commands/Elevator/ElevatorBottom.cpp
@@ -0,0 +1,33 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2017-2018 FIRST. All Rights Reserved.                        */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+#include "commands/Elevator/ElevatorBottom.h"
+#include "Robot.h"
+
+ElevatorBottom::ElevatorBottom() {
+  // Use Requires() here to declare subsystem dependencies
+  // eg. Requires(Robot::chassis.get());
+  Requires(&Robot::Elevator);
+}
+
+// Called just before this Command runs the first time
+void ElevatorBottom::Initialize() {
+  Robot::Elevator.SeekTo(Robot::Elevator.Positions::Bottom);
+}
+
+// Called repeatedly when this Command is scheduled to run
+void ElevatorBottom::Execute() {}
+
+// Make this return true when this Command no longer needs to run execute()
+bool ElevatorBottom::IsFinished() { return true; }
+
+// Called once after isFinished returns true
+void ElevatorBottom::End() {}
+
+// Called when another command which requires one or more of the same
+// subsystems is scheduled to run
+void ElevatorBottom::Interrupted() {}
---
+++ 2019RobotTest/src/main/cpp/commands/Elevator/ElevatorMiddle.cpp
@@ -0,0 +1,33 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2017-2018 FIRST. All Rights Reserved.                        */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+#include "commands/Elevator/ElevatorMiddle.h"
+#include "Robot.h"
+
+ElevatorMiddle::ElevatorMiddle() {
+  // Use Requires() here to declare subsystem dependencies
+  // eg. Requires(Robot::chassis.get());
+  Requires(&Robot::Elevator);
+}
+
+// Called just before this Command runs the first time
+void ElevatorMiddle::Initialize() {
+  Robot::Elevator.SeekTo(Robot::Elevator.Positions::Middle);
+}
+
+// Called repeatedly when this Command is scheduled to run
+void ElevatorMiddle::Execute() {}
+
+// Make this return true when this Command no longer needs to run execute()
+bool ElevatorMiddle::IsFinished() { return true; }
+
+// Called once after isFinished returns true
+void ElevatorMiddle::End() {}
+
+// Called when another command which requires one or more of the same
+// subsystems is scheduled to run
+void ElevatorMiddle::Interrupted() {}
